a9063271122937830c3cbfd23b13c60580e4f017
LinlyBoi <libkyy@e.email>
2023-07-13 21:56:26 +0300

    A fix I believe, not like I tested or anything

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,7 +9,7 @@ int main(void)
 int char_size = sizeof(char);
 printf("Size of a char: %d byte(s)\n",char_size);
 int int_size = sizeof(int);
-printf("Size of a int: %d byte(s)\n",int_size);
+printf("Size of an int: %d byte(s)\n",int_size);
 int long_size = sizeof(long int);
 printf("Size of a long int: %d byte(s)\n",long_size);
 int longlong_size = sizeof(long long int);
