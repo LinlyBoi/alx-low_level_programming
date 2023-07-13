f81ad42ee0e67253fe7e83e4437830c7c7753da5
LinlyBoi <libkyy@e.email>
2023-07-13 21:56:56 +0300

    Only Tom Cruise knows why.

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,14 +7,14 @@
 int main(void)
 {
 int char_size = sizeof(char);
-printf("Size of a char: %d byte(s)\n",char_size);
+printf("Size of a char: %d byte(s)\n", char_size);
 int int_size = sizeof(int);
-printf("Size of an int: %d byte(s)\n",int_size);
+printf("Size of an int: %d byte(s)\n", int_size);
 int long_size = sizeof(long int);
-printf("Size of a long int: %d byte(s)\n",long_size);
+printf("Size of a long int: %d byte(s)\n", long_size);
 int longlong_size = sizeof(long long int);
-printf("Size of a long long int: %d byte(s)\n",longlong_size);
+printf("Size of a long long int: %d byte(s)\n", longlong_size);
 int float_size = sizeof(float);
-printf("Size of a float: %d byte(s)\n",float_size);
+printf("Size of a float: %d byte(s)\n", float_size);
 return (0);
 }
