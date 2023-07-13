7bf02ac457312a0e28aa446ae153b1bd8d111d08
LinlyBoi <libkyy@e.email>
2023-07-13 21:32:15 +0300

    better ignores

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-int main() {
-  char Mystr[] = "\"Programming is like building a multilingual puzzle";
+int main(void)
+{
+char Mystr[] = "\"Programming is like building a multilingual puzzle";
 
-  puts(Mystr);
-
-  return 0;
+puts(Mystr);
+return (0);
 }
