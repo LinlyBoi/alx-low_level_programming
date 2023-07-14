eb7be2e3e9cf91de2139574b458e003b6fe02c60
LinlyBoi <libkyy@e.email>
2023-07-14 23:43:40 +0300

    hoo boy

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Alphabet
+ * Description: nuts
+ * I DONT KNOW
+ * WHAT
+ * YOU
+ * WANT
+ * Return: 0 on success (I never fail)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		if (letter != 'q' && letter != 'e')
+			putchar(letter);
+
+	putchar('\n');
+	return (0);
+}
