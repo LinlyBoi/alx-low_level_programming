0cc99aed05c38dcb24653b0bf438ea7e0ccac50d
LinlyBoi <libkyy@e.email>
2023-07-14 23:42:02 +0300

    things occurred

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Alphabet
+ * Description: nuts
+ * I DONT KNOW
+ * WHAT
+ * YOU
+ * WANT
+ * Return: 0 on success (I never fail)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+	return (0);
+}
