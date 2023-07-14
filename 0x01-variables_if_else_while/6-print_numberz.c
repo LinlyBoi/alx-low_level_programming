c8c0ace7b08d3321f23a0ba26ba667dd344dfd50
LinlyBoi <libkyy@e.email>
2023-07-14 23:49:05 +0300

    Bit Bucket is down. What should I do now?

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Alphabet
+ * Description: nuts
+ * I DONT KNOW
+ * WHAT
+ * YOU
+ * WANT
+ * Return: 0 on success (I never fail)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar(48+num);
+
+	putchar('\n');
+	return (0);
+}
