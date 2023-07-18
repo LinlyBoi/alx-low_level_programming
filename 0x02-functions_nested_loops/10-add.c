dfde00669dc8fbfc0064c1ff4a71bdc33670ea5a
LinlyBoi <libkyy@e.email>
2023-07-18 22:35:52 +0300

    We're no strangers to love

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - two plus two is four
+ * Description: MATH but basic
+ * @a: a
+ * @b: b
+ * Return: result of a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
