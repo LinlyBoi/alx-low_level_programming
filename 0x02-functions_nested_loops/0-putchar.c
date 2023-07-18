d91d40d9d3c05f6d69eaf9ef88dd76bb31f5f999
LinlyBoi <libkyy@e.email>
2023-07-18 20:24:40 +0300

    Fucking egotistical bastard. adds expandtab to vimrc

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,24 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - putchar
  * Description: prints out _putchar
  * Return: 0 (I'm the best)
+ *
+ * _putchar - actually puts out the char
+ * Description: they really want us to do this?
+ * Return: VOID
  */
+#include <wchar.h>
 int main(void)
 {
-	printf("_putchar\n");
+	int i;
+
+	char str[] = "_putchar";
+	for ( i = 0; i < sizeof(str); i++)
+	{
+		_putchar(&str[i]);
+	}
+
 
 	return (0);
 }
