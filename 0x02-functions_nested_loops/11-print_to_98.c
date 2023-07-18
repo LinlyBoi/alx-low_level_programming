67eb8316c396e3254fed84c8e98dbf577bcb93e6
LinlyBoi <libkyy@e.email>
2023-07-18 22:41:15 +0300

    and so the crazy refactoring process sees the sunlight after some months in the dark!

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,9 +11,20 @@ void print_to_98(int n)
 {
 	int i;
 
-	for (i = n; i <= 98; i++)
+	if (n >= 98)
 	{
-		printf("%d, ", i);
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d, ", i);
+		}
 	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d, ", i);
+		}
+	}
+
 	_putchar('\n');
 }
