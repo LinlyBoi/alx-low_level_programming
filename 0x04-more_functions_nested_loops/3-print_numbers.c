7f5038e37ac6d8e2eff37363870d68557b856f06
LinlyBoi <libkyy@e.email>
2023-07-21 01:49:23 +0300

    It's secret!

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+/**
+ * print_numbers: numbers mason
+ * Description: What do they mean?
+ * Return: VOID
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
