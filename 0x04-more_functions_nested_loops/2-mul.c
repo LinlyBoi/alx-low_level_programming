c9b1e57b49e5e07a3994690d73da5928accb3884
LinlyBoi <libkyy@e.email>
2023-07-21 01:40:18 +0300

    Reticulating splines...

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+/**
+ * mul - multiplying
+ * Description: 2 x 2 :)
+ * @a: number juan
+ * @b: his idiot cousin
+ * Return: Value of multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
