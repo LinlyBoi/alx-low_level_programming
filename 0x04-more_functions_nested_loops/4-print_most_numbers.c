079526cbdd7febbaa262171140a782579d25aa54
LinlyBoi <libkyy@e.email>
2023-07-21 01:58:00 +0300

    Popping stash

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_most_numbers - no 2,4
+ * Description: prints almost all numbers
+ * Return: VOID
+ */
+
+void print_most_numbers(void)
+{
+
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		switch (i)
+		{
+			case 2:
+				break;
+			case 4:
+				break;
+			default:
+				_putchar('0' + i);
+		}
+
+	}
+
+}
