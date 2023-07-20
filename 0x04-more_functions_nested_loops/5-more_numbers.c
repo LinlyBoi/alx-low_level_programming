6709236f1954dd2343b5c26bee504d1481d56397
LinlyBoi <libkyy@e.email>
2023-07-21 02:07:53 +0300

    Use a real JS construct, WTF knows why this works in chromium.

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * more_numbers - more
+ * Description: YEAH
+ * Return: VOID
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; j < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			if (j > 9)
+				_putchar('0' + (j / 10));
+
+			_putchar('0' + (j % 10));
+		}
+		_putchar('\n');
+	}
+
+}
