195b35d4a94653332a954a3f25da66b01712db8d
LinlyBoi <libkyy@e.email>
2023-07-21 01:30:33 +0300

    breathe, =, breathe

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+/**
+ * _isdigit - number?
+ * Description: HELP ME
+ * Return: 1 if digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 58)
+		return (1);
+	else
+		return (0);
+}
