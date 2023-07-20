7d92aa16fae0a3dbcb9b050f93ecadfd2a8e525d
LinlyBoi <libkyy@e.email>
2023-07-21 02:42:59 +0300

    The last time I tried this the monkey didn't survive. Let's hope it works better this time.

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,20 +1,21 @@
 #include "main.h"
 /**
- * print_square: square
- * @n: size of square
+ * print_square - square
+ * @size: size of square
  * Description: a square of #
  * Return: void
  */
 void print_square(int size)
 {
 	int i, j;
+
 	if (size <= 0)
 		_putchar('\n');
 	else
 	{
 		for (i = 0; i < size; i++)
 		{
-			for(j = 0; j < size; j++)
+			for (j = 0; j < size; j++)
 				_putchar('#');
 		}
 		_putchar('\n');
