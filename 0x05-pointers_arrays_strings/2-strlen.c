b733a04741cb8d869f409a0e4079e7105b4a26bf
LinlyBoi <libkyy@e.email>
2023-07-24 22:54:41 +0300

    foo

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,16 +1,15 @@
 /**
- * _strlen(char *s) - strlen but worse
+ * _strlen - strlen but worse
  * Description: Get the length of a string
+ * @s: the bozo we counting
  * Return: length of string
  */
 int _strlen(char *s)
 {
 	int idx;
 
-	idx = 0;
-	for(; *(s + idx) != '\0'; idx++)
+
+	for (idx = 0; *(s + idx) != '\0'; idx++)
 
 	return (idx);
-
-
 }
