afe8ceacb87c5e0c75b8c721f6f6cdd63c8e9c57
LinlyBoi <libkyy@e.email>
2023-07-25 01:19:03 +0300

    That last commit message about silly mistakes pales in comparision to this one

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -6,6 +6,15 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	dest = src;
+	int idx, len;
+
+	len = 0;
+	for (; *(src + len) != '\0'; len++)
+		;
+
+	for(idx = 0; idx < len; idx++)
+		*(dest + idx) = *(src + idx);
+	*(dest + len) = '\0';
+
 	return (dest);
 }
