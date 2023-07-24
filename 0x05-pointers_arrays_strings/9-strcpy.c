d62a8ccb6d5055eae44613f05e190bde5f5ba1c1
LinlyBoi <libkyy@e.email>
2023-07-25 01:15:17 +0300

    I know, I know, this is not how I’m supposed to do it, but I can't think of something better.

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strcpy - strcpy at home
+ * @dest: destination to copy
+ * @src: what we copying
+ */
+char *_strcpy(char *dest, char *src)
+{
+	dest = src;
+	return (dest);
+}
