62232b1d6208e20b1efb1237e070a91d05f57f00
LinlyBoi <libkyy@e.email>
2023-07-24 23:45:20 +0300

    Code was clean until manager requested to fuck it up

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,26 +2,24 @@
 /**
  * rev_string - Now draw it in reverse
  * Description: I have done nothing but teleport bread
+ * @s: the string
  * Return: void
  */
 void rev_string(char *s)
 {
 
-	int idx, bdx;
+	int idx, tail;
 	char *rev;
 
 	for (idx = 0; *(s + idx) != '\0'; idx++)
 		;
+
 	idx--;
-	bdx = idx;
-
+	tail = idx;
 	rev = s;
-	for (idx = 0; idx <= bdx; idx++)
-	{
-		rev[idx] = *(s + bdx);
-		bdx--;
-	}
+
+	for (idx = 0; idx <= tail; idx++)
+		rev[idx] = *(s + tail - idx);
+
 	s = rev;
-
-
 }
