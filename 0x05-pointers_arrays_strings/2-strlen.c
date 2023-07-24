8cb24819348fd68971e4c8829361f9f24579bfed
LinlyBoi <libkyy@e.email>
2023-07-24 22:50:04 +0300

    Please no changes this time.

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+/**
+ * _strlen(char *s) - strlen but worse
+ * Description: Get the length of a string
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int idx;
+
+	idx = 0;
+	for(; *(s + idx) != '\0'; idx++)
+
+	return (idx);
+
+
+}
