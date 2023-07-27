e2c1b93116e68d9bcfa6d910e1f9d6d6f75ab2a5
LinlyBoi <libkyy@e.email>
2023-07-27 21:15:11 +0300

    1 done? 0 was done before this dw

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * strncat - strcat but limited
+ * @src: the thing we adding
+ * @dest: the thing we adding to
+ * @n: how many of src we adding
+ * Return: dest after the mutation
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int d_len, s_idx;
+
+	d_len = 0;
+	while (dest[d_len] != '\0')
+		d_len++;
+
+	for (s_idx = 0; s_idx < n; s_idx++)
+		*(dest + d_len + s_idx) = *(src + s_idx);
+
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,4 @@
+# Strings, Pointers, Arrays
+Array: bunch of memory slots behind each other for one thing
+String: An array of chars (char[]) me thinks
+Pointer: Something we point with? Finger
