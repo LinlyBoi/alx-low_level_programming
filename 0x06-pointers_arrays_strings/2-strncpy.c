01fe23e067959a7c29aa14429e5f1798e1285291
LinlyBoi <libkyy@e.email>
2023-07-27 22:29:27 +0300

    (\ /) (O.o) (> <) Bunny approves these changes.

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,20 +9,13 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int d_idx, s_idx;
+	int s_idx;
 
-	d_idx = 0;
-	while (dest[d_idx] != '\0')
-	{
-		*(dest + d_idx) = 0;
-		d_idx++;
-	}
-
-	for (s_idx = 0; s_idx < n; s_idx++)
+	for (s_idx = 0; s_idx < n && *(src + s_idx) != '\0'; s_idx++)
 		*(dest + s_idx) = *(src + s_idx);
 
-	while (d_idx < n)
-		*(dest + d_idx) = '\0';
+	while (dest[s_idx] != '\0' && s_idx < n)
+		*(dest + s_idx++) = '\0';
 
 	return (dest);
 }
