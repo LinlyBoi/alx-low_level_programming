270235357eb2992b53cbee128252ca1c922fcf47
LinlyBoi <libkyy@e.email>
2023-07-27 22:14:42 +0300

    GIT :/

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -15,7 +15,7 @@ char *_strncat(char *dest, char *src, int n)
 	while (dest[d_len] != '\0')
 		d_len++;
 
-	for (s_idx = 0; s_idx < n; s_idx++)
+	for (s_idx = 0; s_idx < n && src[s_idx] != '\0'; s_idx++)
 		*(dest + d_len + s_idx) = *(src + s_idx);
 
 	*(dest + s_idx) = '\0';
