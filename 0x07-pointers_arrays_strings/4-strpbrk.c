83b1af49252993cf5173900a2fb6d17cdafbcd9e
LinlyBoi <libkyy@e.email>
2023-08-01 01:01:38 +0300

    why even bother do things cleanly...

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -2,43 +2,28 @@
 #include <stdio.h>
 
 /**
- * _strchr - find first occurence of character
- * @s: the string
- * @c: charcter to find
- * Return: returns pointer to char or NULL if not exist
- */
-char *_strchr(char *s, char c)
-{
-	int a;
-
-	while (1)
-	{
-		a = *s++;
-		if (a == c)
-		{
-			return (s - 1);
-		}
-		if (a == 0)
-		{
-			return (NULL);
-		}
-	}
-
-}
-/**
- * _strpbrk - finds first occurence but with a filter
- * @s: String
- * @accept: the list
- * Return: pointer to byte where the first occurence happens or NULL if none
+ * *_strpbrk - searches a string for any of a set of bytes
+ * @s: string to search
+ * @accept: stringcontaining the bytes to look for
+ *
+ * Return: pointer to the byte in s that matches one of the bytes in accept
+ * or NULL if no such byte is found
  */
 char *_strpbrk(char *s, char *accept)
 {
-	char i;
+	int i, j;
 
-	do {
-		i = *s++;
-		if (_strchr(accept, i) != NULL)
-			return (s - 1);
-	} while (i != '\0');
+	for (i = 0; *s != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (*s == accept[j])
+			{
+				return (s);
+			}
+		}
+		s++;
+	}
+
 	return (NULL);
 }
