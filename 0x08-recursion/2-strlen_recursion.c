da29899d6cff92e04aded3ced4399de652bf486a
LinlyBoi <libkyy@e.email>
2023-08-02 16:45:46 +0300

    gg ez

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strlen_recursion - getting the legnth of a string but recurring
+ * @s: The string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s)
+	{
+		return (1 + _strlen_recursion(++s));
+	}
+	return (0);
+}
