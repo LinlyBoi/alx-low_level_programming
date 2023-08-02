0eba49b956dfd0d3ce2d5b72cb44769a078a4992
LinlyBoi <libkyy@e.email>
2023-08-02 16:30:20 +0300

    I think now it works

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - print the string backwards
+ * @s: the string
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(++s);
+	}
+	_putchar(*s);
+}
