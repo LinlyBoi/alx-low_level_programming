b93c8a60f5c599ecbb4ef35f690306808c1a1c00
LinlyBoi <libkyy@e.email>
2023-08-04 22:20:19 +0300

    ethan bradberry

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - calling argv[0]
+ * Return: Exit status 0 success, 1 error
+ * @argc: length of arguments
+ * @argv: arguments
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	argc = argc;
+	return (0);
+}
