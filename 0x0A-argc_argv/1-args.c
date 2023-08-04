70f9f1af50c40bb27baf8707f2b122c9572e263b
LinlyBoi <libkyy@e.email>
2023-08-04 22:23:05 +0300

    1 done? YES

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - calling argv[0]
+ * Return: Exit status 0 success, 1 error
+ * @argc: length of arguments
+ * @argv: arguments
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	argv[0] = argv[0];
+	return (0);
+}
