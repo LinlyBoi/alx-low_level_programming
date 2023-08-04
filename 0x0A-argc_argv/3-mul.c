03e819226613eedea299ac6b927ce72002f1164f
LinlyBoi <libkyy@e.email>
2023-08-05 01:16:13 +0300

    I multiply BIGger numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+int numerate(char *s);
 
 /**
  * main - multiplication program
@@ -10,11 +11,36 @@
 int main(int argc, char *argv[])
 {
 	if (argc == 3)
-		printf("%d\n", (*argv[1] - '0') * (*argv[2] - '0'));
+		printf("%d\n", (numerate(argv[1]) * numerate(argv[2])));
 	else
 	{
 		printf("Error\n");
 		return (1);
 	}
 	return (0);
+}
+
+/**
+ * numerate - String -> number
+ * @s: Stinky string
+ * Return: Int of string or 0 if invalid
+ */
+int numerate(char *s)
+{
+	int sum;
+
+	sum = 0;
+	while (*s)
+	{
+		if (*s >= '0' && *s <= '9')
+		{
+			sum += *s - '0';
+			sum *= 10;
+			s++;
+		}
+		else
+			break;
+
+	}
+	return (sum);
 }
