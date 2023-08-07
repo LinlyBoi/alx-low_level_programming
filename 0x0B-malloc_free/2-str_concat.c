2554f839187a49caba431c85a99340978e25cba9
LinlyBoi <libkyy@e.email>
2023-08-07 17:04:39 +0300

    help?

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -36,7 +36,10 @@ char *str_concat(char *s1, char *s2)
 		*(str + i++) = *(s1++);
 	while (*s2)
 		*(str + i++) = *(s2++);
-	*(str + i) = '\0';
+	if (i > 0)
+		*(str + i) = '\0';
+	else
+		str = "";
 
 	return (str);
 
