306d9e4cf4ef3be9de4ffac87a8341aeb6c26ed9
LinlyBoi <libkyy@e.email>
2023-08-07 17:39:47 +0300

    HELP

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,12 +12,16 @@ char *str_concat(char *s1, char *s2)
 	char *str;
 	int i, size;
 
+	size = 0;
 	if (s1 == NULL)
 		s1 = "";
+	else
+		size++;
 	if (s2 == NULL)
 		s2 = "";
+	else
+		size++;
 
-	size = 0;
 	while (*(s1 + size))
 		size++;
 	while (*(s2 + size))
@@ -32,9 +36,9 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 
 	i = 0;
-	while (*s1 != '\0')
+	while (*s1)
 		*(str + i++) = *(s1++);
-	while (*s2 != '\0')
+	while (*s2)
 		*(str + i++) = *(s2++);
 
 	*(str + i) = '\0';
