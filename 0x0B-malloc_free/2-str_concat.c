9f35c7fef1f87f8ab53a17964249a1a47c704891
LinlyBoi <libkyy@e.email>
2023-08-07 17:00:40 +0300

    me stinky

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - cat? 2 strings? meow?
+ * @s1: String juan
+ * @s2: String does
+ * Return: pointer to new string that is s1 s2 combined :)
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *str;
+	int i, size;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	size = 0;
+	while (*(s1 + size))
+		size++;
+	while (*(s2 + size))
+		size++;
+
+	if (size > 0)
+		str = malloc(size * sizeof(char));
+	else
+		return (NULL);
+
+	if (str == NULL)
+		return (NULL);
+
+	i = 0;
+	while (*s1)
+		*(str + i++) = *(s1++);
+	while (*s2)
+		*(str + i++) = *(s2++);
+	*(str + i) = '\0';
+
+	return (str);
+
+}
