33576bb174ed4518c0f6a1a827b8f413466736f8
LinlyBoi <libkyy@e.email>
2023-08-11 01:43:57 +0300

    maybe fix?

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,7 +11,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *chonky;
-	unsigned int i;
+	unsigned int i, j;
 
 	if (n <= sizeof(*s2))
 		chonky = malloc(sizeof(*s1) + n);
@@ -21,10 +21,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (chonky)
 	{
 		i = 0;
+		j = 0;
 		while (s1 && *s1)
-			*(chonky + i++) = *(s1++);
+			*(chonky + i++) = *(s1 + j++);
+		j = 0;
 		while (s2 && *s2)
-			*(chonky + i++) = *(s2++);
+			*(chonky + i++) = *(s2 + j++);
 
 		return (chonky);
 	}
