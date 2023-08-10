6613fb3e6adbae5e5e8d81cf9c3b36424cdb2899
LinlyBoi <libkyy@e.email>
2023-08-11 01:41:43 +0300

    concating time

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include "stdlib.h"
+
+/**
+ * string_nconcat - how many times we gotta do this old man
+ * @s1: First String
+ * @s2: String to add at end of s1
+ * @n: how many bytes of s2 we adding
+ * Return: pointer with new str / NULL on failure
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *chonky;
+	unsigned int i;
+
+	if (n <= sizeof(*s2))
+		chonky = malloc(sizeof(*s1) + n);
+	else
+		chonky = malloc(sizeof(*s1) + sizeof(*s2) - 1);
+
+	if (chonky)
+	{
+		i = 0;
+		while (s1 && *s1)
+			*(chonky + i++) = *(s1++);
+		while (s2 && *s2)
+			*(chonky + i++) = *(s2++);
+
+		return (chonky);
+	}
+	else
+		return (NULL);
+}
