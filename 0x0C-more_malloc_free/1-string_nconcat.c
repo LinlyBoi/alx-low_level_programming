d0f9df06bd415bf8366c0c2495b0e06ebc9044ff
LinlyBoi <libkyy@e.email>
2023-08-11 02:03:39 +0300

    PLEASE

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -26,10 +26,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 			*(chonky + i) = *(s1 + i);
 			i++;
 		}
-		for (j = 0; j < n; j++)
-			*(chonky + i++) = *(s2 + j);
+		if (s2)
+		{
+			for (j = 0; j < n; j++)
+				*(chonky + i++) = *(s2 + j);
+		}
 
-		*(chonky + i) = '\0';
+                *(chonky + i) = '\0';
 		return (chonky);
 	}
 	else
