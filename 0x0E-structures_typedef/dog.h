1cd61bba1a75050409e9f1e8cee1acd87051020b
LinlyBoi <libkyy@e.email>
2023-08-15 00:22:18 +0300

    WHY DID I NOT COMMIT THI

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,6 +20,6 @@ struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
-char *_strdup(char *str);
+char *_strcpy(char *dest, char *src);
 
 #endif
