ffa7e2424b6e6c3ef7aadc11911c2a9cb3330a9f
LinlyBoi <libkyy@e.email>
2023-08-15 00:55:22 +0300

    the benefnign

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -22,5 +22,6 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 char *_strcpy(char *dest, char *src);
 unsigned int _strlen(char *str);
+void free_dog(dog_t *d);
 
 #endif
