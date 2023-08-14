05a9f83af12d55d0b76ef6182fad47402b7c69c0
LinlyBoi <libkyy@e.email>
2023-08-14 23:46:41 +0300

    7 is the number format

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,6 +9,6 @@ void print_dog(struct dog *d)
 	if (!d)
 		return;
 	printf("Name: %s\n", d->name != NULL ? d->name : "(nil)");
-	d->age ? printf("Age: %.5f\n", d->age) : printf("nill");
+	d->age ? printf("Age: %.6f\n", d->age) : printf("nill");
 	printf("Owner: %s\n", d->owner != NULL ? d->owner : "(nil)");
 }
