0db5f1ae22d48171985e4ce3410fef1a53de7532
LinlyBoi <libkyy@e.email>
2023-08-16 23:35:45 +0300

    betty

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,6 +11,6 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	int i;
 
 	if (array && size && action)
-		for(i = 0; i < (int) size; i++)
+		for (i = 0; i < (int) size; i++)
 			(*action)(*(array + i));
 }
