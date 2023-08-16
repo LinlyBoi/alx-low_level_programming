e8f7ef7212b13dd92d30c88f25e5259030ca5679
LinlyBoi <libkyy@e.email>
2023-08-16 23:36:15 +0300

    my bad

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,8 @@
+#include <stdlib.h>
+
 #ifndef __MAIN__
 #define __MAIN__
 void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
