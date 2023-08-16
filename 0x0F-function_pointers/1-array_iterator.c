86da1f8d00997b31ed253891e3772de489e9650b
LinlyBoi <libkyy@e.email>
2023-08-16 23:30:49 +0300

    fr fr short code

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - iterate over array with a function
+ * @array: really?
+ * @size: Please man
+ * @action: what to do to the fellas above
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	for(i = 0; i < size; i++)
+		(*action)(*(array + i));
+}
