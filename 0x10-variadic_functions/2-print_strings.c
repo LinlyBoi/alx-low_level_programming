911e4642a605dcfde1d44eb539603267954e1a58
LinlyBoi <libkyy@e.email>
2023-08-17 22:50:21 +0300

    logically correct probably

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_strings - prints a bunch of strings wow
+ * @separator: separates strings skipped if null
+ * @n: number of strings
+ * @...: strings
+ */
+ void print_strings(const char *separator, const unsigned int n, ...)
+ {
+	 va_list words;
+	 int idx;
+	 char *next;
+
+	 for (idx = 0; idx < n; idx++)
+	 {
+		 next = va_arg(words, char*);
+		 if (!next)
+			 next = "(nil)";
+		 if (!separator)
+			 printf("%s", next);
+		 else if (separator && idx == 0)
+		 {
+			 printf("%s", next);
+
+		 }
+		 else
+			 printf("%s%s", separator, next);
+	 }
+ }
