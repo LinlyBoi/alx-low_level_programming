cdc62e018cac182e89ab66e73bb9e6c0664d409f
LinlyBoi <libkyy@e.email>
2023-08-24 18:20:58 +0300

    yeah?

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -10,16 +10,20 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_tail, *temp;
 
-	temp = *head;
-	if (temp->next)
-		add_node_end(&(temp->next), str);
 
 	new_tail = malloc(sizeof(list_t));
 	if (!new_tail || !str)
 		return (NULL);
-
 	new_tail->str = strdup(str);
 	new_tail->len = strlen(str);
-	temp->next = new_tail;
+	if (!head)
+		*head = new_tail;
+	else
+	{
+		temp = *head;
+		while(temp->next)
+			temp = temp->next;
+		temp->next = new_tail;
+	}
 	return (new_tail);
 }
