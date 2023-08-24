558f387491515558560979e73a2f238f37e89b82
LinlyBoi <libkyy@e.email>
2023-08-24 17:25:23 +0300

    this ain't functional programming

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <string.h>
+/**
+ * add_node - add node at the start
+ * @str: string
+ * @head: list head
+ * Return: address of new element or NULL
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_head;
+
+	new_head = malloc(sizeof(list_t));
+	if (!new_head)
+		return(NULL);
+
+	new_head->str = strdup(str);
+	new_head->len = strlen(str);
+	if (head)
+		new_head->next = *head;
+
+	*head = new_head;
+
+	return (new_head);
+}
