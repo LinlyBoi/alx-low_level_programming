25c63d63c830f55eb7d293825389be08ccd5082c
LinlyBoi <libkyy@e.email>
2023-08-29 20:11:20 +0300

    handled idx = 0

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,15 +13,22 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i;
 
 	new_node = malloc(sizeof(listint_t));
-	if (!new_node)
+	if (!new_node || !head)
 		return (NULL);
 
 	current = *head;
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
 	for (i = 0; i < idx - 1 && current; i++)
 		current = current->next;
 
 	if (!current)
 		return (NULL);
+
 
 	new_node->n = n;
 	new_node->next = current->next;
