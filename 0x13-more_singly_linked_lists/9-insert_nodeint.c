b1ef0e217595aed9733fddfd1de3770c81f3943a
LinlyBoi <libkyy@e.email>
2023-08-29 20:16:11 +0300

    I forgot to SAVE?!

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,13 +16,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (!new_node || !head)
 		return (NULL);
 
-	current = *head;
+	new_node->n = n;
 	if (idx == 0)
 	{
 		new_node->next = *head;
 		*head = new_node;
 		return (new_node);
 	}
+	current = *head;
 	for (i = 0; i < idx - 1 && current; i++)
 		current = current->next;
 
@@ -30,7 +31,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 
 
-	new_node->n = n;
 	new_node->next = current->next;
 	current->next = new_node;
 	return (new_node);
