9db8943ecf64053b2d59e85068d98d3f4772cd67
LinlyBoi <libkyy@e.email>
2023-08-29 18:30:42 +0300

    ???

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,14 +12,16 @@ size_t print_listint(const listint_t *h)
 	int count;
 
 	count = 0;
-	if (h == NULL)
-		return (0);
-
-	while (h)
+	if (h != NULL)
 	{
-		printf("%d\n", h->n);
-		h = h->next;
-		count++;
+		while (h)
+		{
+			printf("%d\n", h->n);
+			h = h->next;
+			count++;
+		}
+
 	}
+
 	return (count);
 }
