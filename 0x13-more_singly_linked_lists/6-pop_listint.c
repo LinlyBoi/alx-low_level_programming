d2573db65bd810e17224ce1f6bd58dea19c6f28e
LinlyBoi <libkyy@e.email>
2023-08-29 19:19:01 +0300

    forgot to free

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,12 +9,13 @@ int pop_listint(listint_t **head)
 	listint_t *temp;
 	int n;
 
-	if (!*head)
+	if (!*head || !head)
 		return (0);
 
-	temp = *head;
-	n = temp->n;
-	temp = temp->next;
+	n = (*head)->n;
+	temp = *head->next;
+	free(*head);
+	*head = temp;
 
 	return (n);
 }
