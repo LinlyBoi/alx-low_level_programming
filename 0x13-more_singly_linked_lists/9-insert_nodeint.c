d6f29a448cbc4a63db95799cbb99fc766f96b541
LinlyBoi <libkyy@e.email>
2023-08-29 19:54:04 +0300

    typo

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * listint_t *insert_nodeint_at_index - yes
+ * @head: head of list
+ * @idx: index at insert at
+ * @n: value to insert
+ * Return: address to new node or NULL
+ */
+listint_t *insert_nodeint_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *current, *new_node;
+	unsigned int i;
+
+	new_node = malloc(sizeof(listint_t));
+	if (!new_node)
+		return NULL;
+
+	current = *head;
+	for (i = 0; i < idx - 1 && current; i++)
+		current = current->next;
+
+	if (!current)
+		return NULL;
+
+	new_node->next = current->next;
+	current->next = new_node;
+	return (new_node);
+}
