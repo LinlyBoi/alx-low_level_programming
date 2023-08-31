91107a3e495f437e4186fbfd6aa1ba1ef1873c46
LinlyBoi <libkyy@e.email>
2023-09-01 00:38:24 +0300

    oh yeah

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,5 +7,6 @@ unsigned int resolve_binary(const char *b, unsigned int power);
 unsigned int _strlen(const char *str);
 void print_binary(long int n);
 int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
 
 #endif
