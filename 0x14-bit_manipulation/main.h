4e0e3c049d491545735f16b612b1ca5773e0ad56
LinlyBoi <libkyy@e.email>
2023-09-01 00:42:55 +0300

    forgot prototypes srry

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -8,5 +8,7 @@ unsigned int _strlen(const char *str);
 void print_binary(long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
 
 #endif
