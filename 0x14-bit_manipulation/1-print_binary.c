15053df2a5ec79dec966712c20e1b25c4dc8221d
LinlyBoi <libkyy@e.email>
2023-08-31 16:01:52 +0300

    Yeah?

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,5 +6,13 @@
  */
 void print_binary(long n)
 {
+	unsigned int shifts, temp;
 
+	temp = n;
+	for (shifts = 0; temp > 0; shifts++)
+		temp = temp >> 1;
+
+	/* (n >> 1) & 1 */
+	for (temp = shifts; temp > 0; temp--)
+		((n >> temp) & 1) ? _putchar('1') : _putchar('0');
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 unsigned int resolve_binary(const char *b, unsigned int power);
 unsigned int _strlen(const char *str);
